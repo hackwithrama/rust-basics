f646d658d13ade5e83898cb5ec8542191c3c9dcd
Ramachandran Varadaraju <rajram92@hotmail.com>
2025-06-30 05:55:25 +0530

    updated

---
+++ src/helpers.rs
@@ -0,0 +1,4 @@
+pub fn get_full_name(first: &str, last: &str) -> String {
+    let full_name = format!("{0} {1}", first, last);
+    return full_name;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+pub mod helpers;
+
+use crate::helpers::get_full_name;
+
 fn main() {
     println!("Hello, world!");
+    let my_result = get_full_name("Ramachandran", "Ramakrishnan");
+    println!("{}", my_result);
 }
